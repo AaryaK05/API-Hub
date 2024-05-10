a729da260debabb4c1110623caa24c933b543ec0
Aarya Kasbe <aaryakasbe05@gmail.com>
2024-05-10 20:18:02 +0530

    Subfolder issues

---
+++ Github-Profile-Finder/src/App.js
@@ -0,0 +1,14 @@
+
+
+import GithubProfileFinder from './components/GithubProfileFinder';
+
+function App() {
+  return (
+    <div className="App">
+    <GithubProfileFinder/>
+    </div>
+  );
+}
+
+
+export default App;
